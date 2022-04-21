2b2b34ebf378d25c95ec79a79f80b6b3749bf5d1
tgsiegel <tgsiegel@umd.edu>
2022-04-20 21:20:50 -0400

    complete ui tests

--- src/components/Fleet/plugins/plugins.test.tsx
+++ src/components/Fleet/plugins/plugins.test.tsx
@@ -1,89 +1,90 @@
 import * as React from 'react';
-import {render} from '@testing-library/react'
+import { render } from '@testing-library/react';
 import { Point } from 'pixi.js-legacy';
-import '@testing-library/jest-dom'
-import WS from "jest-websocket-mock";
-import FleetManager from "../FleetManager"
-import FleetGroup from "../FleetGroup"
-import click from "./click"
+import '@testing-library/jest-dom';
+import WS from 'jest-websocket-mock';
+import FleetManager from '../FleetManager';
+import FleetGroup from '../FleetGroup';
+import click from './click';
 import { FleetSprite } from '../helper';
 import hover from './hover';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const server = new WS("ws://localhost:9095/ws/v1/fleet");
+const server = new WS('ws://localhost:9095/ws/v1/fleet');
 
 const getFm = () => {
-    const ele = render(<div id="fleet" />).container as HTMLDivElement
-    ele.style.width = "100px"
-    ele.style.height = "100px"
-    const fm = new FleetManager(ele)
+    const ele = render(<div id="fleet" />).container as HTMLDivElement;
+    ele.style.width = '100px';
+    ele.style.height = '100px';
+    const fm = new FleetManager(ele);
 
-    fm.connect()
+    fm.connect();
 
     fm.groups = {
-        "g": new FleetGroup("g", "g", {x: 1, y: 1})
-    }
+        g: new FleetGroup('g', 'g', { x: 1, y: 1 }),
+    };
 
-    fm.groups.g.allIds = ["asdf"]
-    fm.groups.g.rowSize = 10
+    fm.groups.g.allIds = ['asdf'];
+    fm.groups.g.rowSize = 10;
 
-    const child = new FleetSprite()
-    child.name = "asdf"
-    child.data = {
+    const child = new FleetSprite();
+    child.name = 'asdf';
+    child.setData({
         meta: {
-            uid: "asdf",
-            namespace: "asdf",
-            name: "asdf",
+            uid: 'asdf',
+            namespace: 'asdf',
+            name: 'asdf',
             details: {
-                "asdf": "asdf"
+                asdf: 'asdf',
             },
         },
-        dimension: "pod",
+        dimension: 'pod',
         status: {
-            color: [0,0,0],
-            reason: "Running",
-            value: "asd"
-        }
-    }
-    fm.addChild(child)
+            color: [0, 0, 0],
+            reason: 'Running',
+            value: 'asd',
+        },
+    });
 
-    return {fm, ele}
-}
+    fm.addChild(child);
+
+    return { fm, ele };
+};
 
 test('can handle click', async () => {
-    const {fm} = getFm()
+    const { fm } = getFm();
     fm.groupRegions = {
-        "g": {
+        g: {
             x: {
                 start: 0,
-                end: 100
+                end: 100,
             },
             y: {
                 start: 0,
-                end: 100
-            }
-        }
-    }
-    const point = new Point(45, 1)
-    click(fm, {x:45, y:1}, point)
+                end: 100,
+            },
+        },
+    };
+    const point = new Point(45, 1);
+    click(fm, { x: 45, y: 1 }, point);
 });
 
 test('can handle hover', async () => {
-    const {fm} = getFm()
+    const { fm } = getFm();
     fm.groupRegions = {
-        "g": {
+        g: {
             x: {
                 start: 0,
-                end: 100
+                end: 100,
             },
             y: {
                 start: 0,
-                end: 100
-            }
-        }
-    }
-    const point = new Point(45, 1)
-    const fn = jest.fn()
-    hover(fm, {x:45, y:1}, fn, point)
-    expect(fn).toHaveBeenCalled()
+                end: 100,
+            },
+        },
+    };
+    const point = new Point(45, 1);
+    const fn = jest.fn();
+    hover(fm, { x: 45, y: 1 }, fn, point);
+    expect(fn).toHaveBeenCalled();
 });
