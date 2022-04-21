fc91f9b9336286050eba76f938e0d8c68f92d546
tgsiegel <tgsiegel@umd.edu>
2022-04-20 21:21:20 -0400

    clean

--- src/views/DeploymentList/DeploymentList.test.tsx
+++ src/views/DeploymentList/DeploymentList.test.tsx
@@ -1,39 +1,42 @@
 import * as React from 'react';
 import { MemoryRouter, Route, Routes } from 'react-router';
-import {render,fireEvent} from '@testing-library/react'
-import {rest} from 'msw'
-import {setupServer} from 'msw/node'
-import '@testing-library/jest-dom'
+import { render } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import '@testing-library/jest-dom';
 import Layout from '../../layouts/Layout';
-import Deployments from '../../services/k8/deployment.service'; 
+import Deployments from '../../services/k8/deployment.service';
 import DeploymentList from './DeploymentList';
 import { generateDeployment } from '../../testing/type_mocks';
 
 const server = setupServer(
     rest.get(`${Deployments.base}/*`, (req, res, ctx) => {
-        const count = 20 
-        const items = []
-        for (let i = 0; i < count; i+=1) {
-            items.push(generateDeployment("asdf"))
+        const count = 20;
+        const items = [];
+        for (let i = 0; i < count; i += 1) {
+            items.push(generateDeployment('asdf'));
         }
-        return res(ctx.json({
-            items,
-            total: items.length
-        }))
-    }
-    ),
-)
+        return res(
+            ctx.json({
+                items,
+                total: items.length,
+            })
+        );
+    })
+);
 
-beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 test('renders without crashing', async () => {
-    render(<MemoryRouter initialEntries={['/']}>
-        <Routes>
-            <Route path="/" element={<Layout />}>
-                <Route path="" element={<DeploymentList />} />
-            </Route>
-        </Routes>
-    </MemoryRouter>)
+    render(
+        <MemoryRouter initialEntries={['/']}>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route path="" element={<DeploymentList />} />
+                </Route>
+            </Routes>
+        </MemoryRouter>
+    );
 });
