756aa5992085008d3d7dc421325f129c7ad99345
tgsiegel <tgsiegel@umd.edu>
2022-04-22 22:24:07 -0400

    update

--- lib/kubernetes/types/comparable_test.go
+++ lib/kubernetes/types/comparable_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestComparableString(t *testing.T) {
+func TestComparable1(t *testing.T) {
 	testCases := []struct {
 		name     string
 		v1       Comparable
@@ -16,44 +16,81 @@ func TestComparableString(t *testing.T) {
 	}{
 		{
 			name:     "test1",
-			v1:       ComparableString("a"),
-			v2:       ComparableString("b"),
+			v1:       getComparable("a"),
+			v2:       getComparable("b"),
 			operator: EqualOperator,
 			expected: false,
 		},
 		{
 			name:     "test2",
-			v1:       ComparableString("a"),
-			v2:       ComparableString("b"),
+			v1:       getComparable("a"),
+			v2:       getComparable("b"),
 			operator: GreaterThanOperator,
 			expected: false,
 		},
 		{
 			name:     "test3",
-			v1:       ComparableString("b"),
-			v2:       ComparableString("b"),
+			v1:       getComparable("b"),
+			v2:       getComparable("b"),
 			operator: GreaterThanOrEqualOperator,
 			expected: true,
 		},
 		{
 			name:     "test4",
-			v1:       ComparableString("b"),
-			v2:       ComparableString("b"),
+			v1:       getComparable("b"),
+			v2:       getComparable("b"),
 			operator: NotEqualOperator,
 			expected: false,
 		},
 		{
 			name:     "test4",
-			v1:       ComparableString("c"),
-			v2:       ComparableString("b"),
+			v1:       getComparable("c"),
+			v2:       getComparable("b"),
 			operator: LessThanOrEqualOperator,
 			expected: false,
 		},
 		{
 			name:     "test4",
-			v1:       ComparableString("b"),
-			v2:       ComparableString("c"),
+			v1:       getComparable("b"),
+			v2:       getComparable("c"),
 			operator: LessThanOperator,
+			expected: true,
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equal(t, test.operator.Test(test.v1, test.v2), test.expected)
+		})
+	}
+}
+func TestComparable2(t *testing.T) {
+	testCases := []struct {
+		name     string
+		v1       Comparable
+		v2       Comparable
+		operator Operator
+		expected bool
+	}{
+		{
+			name:     "test1",
+			v1:       getComparable(int64(32)),
+			v2:       getComparable(int32(32)),
+			operator: EqualOperator,
+			expected: true,
+		},
+		{
+			name:     "test2",
+			v1:       getComparable(80),
+			v2:       getComparable(2),
+			operator: GreaterThanOrEqualOperator,
+			expected: true,
+		},
+		{
+			name:     "test3",
+			v1:       getComparable(80),
+			v2:       getComparable(200),
+			operator: LessThanOrEqualOperator,
 			expected: true,
 		},
 	}
