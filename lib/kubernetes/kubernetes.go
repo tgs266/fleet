717ded103084fb68153b819f6890cb40b4e7f914
tgsiegel <tgsiegel@umd.edu>
2022-04-26 15:52:50 -0400

    add error condition

--- lib/kubernetes/kubernetes.go
+++ lib/kubernetes/kubernetes.go
@@ -1,6 +1,7 @@
 package kubernetes
 
 import (
+	"github.com/tgs266/fleet/lib/errors"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	metrics "k8s.io/metrics/pkg/client/clientset/versioned"
@@ -13,6 +14,9 @@ type K8Client struct {
 }
 
 func (K8 *K8Client) Connect(config *rest.Config) error {
+	if config == nil {
+		return errors.NewConfigInitializationError(nil)
+	}
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		return err
