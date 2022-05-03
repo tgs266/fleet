108a521993a60a29e1de7d2394f150517eebdcea
tgsiegel <tgsiegel@umd.edu>
2022-05-03 12:09:48 -0400

    fix tests

--- src/views/DeploymentDetails/Tabs/Git.tsx
+++ src/views/DeploymentDetails/Tabs/Git.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import GitControlBar from '../../../components/Git/GitTabBar';
+import GitControlBar from '../../../components/Git/GitControlBar';
 
 export default function Git() {
     return <GitControlBar />;
