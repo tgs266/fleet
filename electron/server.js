73d40be43a3c5a802044fb0d31e168c0508da446
tgsiegel <tgsiegel@umd.edu>
2022-05-11 18:18:11 -0400

    update

--- electron/server.js
+++ electron/server.js
@@ -1,17 +1,23 @@
+/* eslint-disable no-restricted-syntax */
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const net = require('net');
 const homedir = require('os').homedir();
+const bodyParser = require('body-parser');
 const k8s = require('@kubernetes/client-node');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
 
 app.get('/api/v1/electron/detect', (req, res) => {
     try {
         const rawYaml = fs.readFileSync(path.join(homedir, '.kube', 'config'));
         const kubeconfig = new k8s.KubeConfig();
         kubeconfig.loadFromString(rawYaml.toString());
-        res.status(200).json(kubeconfig.clusters);
+        res.status(200).json(kubeconfig.contexts);
     } catch {
         res.status(404).json({
             status: 'NO_FILE_FOUND',
@@ -19,6 +25,61 @@ app.get('/api/v1/electron/detect', (req, res) => {
             message: 'could not find kubernetes config file',
         });
     }
+});
+
+app.post('/api/v1/electron/connect', (req, res) => {
+    let kubeconfig = null;
+    try {
+        const rawYaml = fs.readFileSync(path.join(homedir, '.kube', 'config'));
+        kubeconfig = new k8s.KubeConfig();
+        kubeconfig.loadFromString(rawYaml.toString());
+    } catch {
+        res.status(404).json({
+            status: 'NO_FILE_FOUND',
+            code: 404,
+            message: 'could not find kubernetes config file',
+        });
+        return;
+    }
+
+    const context = req.body;
+    kubeconfig.setCurrentContext(context.name);
+
+    const k8sApi = kubeconfig.makeApiClient(k8s.CoreV1Api);
+    k8sApi
+        .listNamespacedPod('fleet')
+        .then((r) => {
+            for (const pod of r.body.items) {
+                if (pod.metadata.name.includes('fleet')) {
+                    const forward = new k8s.PortForward(kubeconfig);
+                    const server = net.createServer((socket) => {
+                        forward.portForward(
+                            'fleet',
+                            pod.metadata.name,
+                            [9095],
+                            socket,
+                            null,
+                            socket
+                        );
+                    });
+                    server.listen(9095, '127.0.0.1');
+                    res.json('connected');
+                    return;
+                }
+            }
+            res.status(404).json({
+                status: 'NO_FLEET_DEP_FOUND',
+                code: 404,
+                message: 'could not find fleet deployment on cluster',
+            });
+        })
+        .catch(() => {
+            res.status(404).json({
+                status: 'NO_FLEET_DEP_FOUND',
+                code: 404,
+                message: 'could not find fleet deployment on cluster',
+            });
+        });
 });
 
 app.listen(9096);
