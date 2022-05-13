8bf69d27412812fdd517087575b104ccf20b4a72
Tucker Siegel <tgsiegel@terpmail.umd.edu>
2022-05-13 07:37:44 -0400

    update full dep

--- deploy/full-deploy.sh
+++ deploy/full-deploy.sh
@@ -2,3 +2,14 @@
 
 kubectl create namespace fleet
 kubectl create namespace fleet-metrics
+
+curl -L https://api.github.com/repos/tgs266/fleet/tarball -o fleet.tgz
+tar xzvf fleet.tgz 
+cd fleet 
+cd deploy 
+kubectl apply -f aio.yaml
+kubectl apply -f prometheus/
+kubectl apply -f node-exporter/
+kubectl apply -f kube-state-metrics/
+cd ../..
+rm -rf fleet fleet.tgz
